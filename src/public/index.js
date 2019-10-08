8189943334385eb78e5513731f5c72c61e32382d
Sal Tamay <sal.tamay@outlook.com>
2019-10-08 13:09:06 -0400

    Succesful API call - Make calls to Aylien API with their SDK on the server instead of   client which get rids of the CORS error. - Make client side async call to the server to receive the response from   Aylien API

--- src/public/index.js
+++ src/public/index.js
@@ -6,34 +6,30 @@ import './css/style.scss';
 
 console.log('Entry point is successfully setup'); 
 
-test();
+// test();
 
-const makeAPICall = async () => {
-  const data = {
-    mode: "tweet",
-    text: "John is a very good player"
-  }
+document.getElementById('submit').addEventListener('click', (e) => {
+  e.preventDefault();
+  onSubmitText();
+})
+
+const onSubmitText = async () => {
+  const text = document.getElementById('user-input').value;
   try {
-    const response = await fetch('https://api.aylien.com/api/v1/sentiment',
+    const response = await fetch('http://localhost:8080/sentiment',
     {
       method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        "X-AYLIEN-TextAPI-Application-ID": "30e293c0",
-        "X-AYLIEN-TextAPI-Application-Key": "2848c8941cc4d6ae426d9239d9a3d522",
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(data)
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({text: text})
     });
     if (response.ok) {
-      console.log(response);
+      const jsonRes = await response.json();
+      console.log(jsonRes); 
     }
   } catch(error) {
     console.log(error);
   }
 }
-
-makeAPICall();
 
 
 
--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const AYLIENTextAPI = require('aylien_textapi');
+
 const app = express();
 
 app.use(cors());
@@ -25,5 +27,30 @@ const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`CORS-enabled web server listening on port ${PORT}!`);
 })
+
+const textapi = new AYLIENTextAPI({
+  application_id: "30e293c0",
+  application_key: "2848c8941cc4d6ae426d9239d9a3d522"
+});
+
+
+
+app.post('/sentiment', (req, res, next) => {
+  if(req.body.text !== " ") {
+    const text = req.body.text;
+    textapi.sentiment({
+      'text': text
+    }, function (error, response) {
+      if (error === null) {
+        res.status(201).send(response);
+      }
+    });
+  } else {
+    res.status(400).json('Bad Request');
+  }
+})
+
+
+
 
 
